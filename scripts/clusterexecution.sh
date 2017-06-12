77a6e7888930c95863c597f942d9195192a7f522
SrikalaRekapalli <srekapalli@sysgain.com>
2017-06-12 21:39:00 +0530

    Create clusterexecution.sh

--- scripts/clusterexecution.sh
+++ scripts/clusterexecution.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 adminUsername=$1
 adminPassword=$2
-cloudera-director bootstrap-remote azure.simple.expanded.conf --lp.remote.username=$1 --lp.remote.password=$2 --lp.remote.hostAndPort=$1:7189
+host=$3
+cloudera-director bootstrap-remote azure.simple.expanded.conf --lp.remote.username=$1 --lp.remote.password=$2 --lp.remote.hostAndPort=$3:7189
