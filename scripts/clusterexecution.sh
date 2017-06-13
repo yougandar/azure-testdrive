d69b3b2fd6c0f9f5e49ce8f2ebd612391ccca69d
unknown <omanduri@sysgain.com>
2017-06-13 17:10:49 +0530

    up

--- scripts/clusterexecution.sh
+++ scripts/clusterexecution.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
+sudo -i
 dirUsername=$1
 dirPassword=$2
 host=$3
-cd /home/azureuser
-sudo chmod 777 azure.simple.expanded.conf
 cloudera-director bootstrap-remote azure.simple.expanded.conf --lp.remote.username="$1" --lp.remote.password="$2" --lp.remote.hostAndPort="$3":7189
