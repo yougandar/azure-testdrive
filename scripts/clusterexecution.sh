6717f6fe792ddecbd7f467adbc817a219f917ccc
ooha-m <omanduri@sysgain.com>
2017-06-22 14:36:45 +0530

    Delete clusterexecution.sh

--- scripts/clusterexecution.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-DirUserName=$1
-Dirpassword=$2
-HostName=$3
-DirUrl="$HostName:7189"
-Confpath='/home/clouderauser/azure.simple.expanded.conf'
-cloudera-director bootstrap-remote $Confpath --lp.remote.username=$1 --lp.remote.password=$2 --lp.remote.hostAndPort=$DirUrl
-#cloudera-director bootstrap-remote $Confpath --lp.remote.username=$DirUserName --lp.remote.password=$Dirpassword --lp.remote.hostAndPort=$DirUrl
