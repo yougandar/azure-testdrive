c0d00f8b6f58451ebcfa6a8e6a11835bf7cda1d0
SrikalaRekapalli <srekapalli@sysgain.com>
2017-06-22 18:00:38 +0530

    Update hdfsdatalakesetup.sh

--- scripts/hdfsdatalakesetup.sh
+++ scripts/hdfsdatalakesetup.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+adminpassword=$1
 MangerIP="10.3.0.5"
 curl -u admin:admin 'http://'${MangerIP}':7180/api/v1/clusters/Director_Azure_Deployment/services' > /tmp/ClouderaServices
 cat /tmp/ClouderaServices  | grep 'serviceUrl' | awk -F'/' '{print $6}' | tr -d '",' > /tmp/CServices
