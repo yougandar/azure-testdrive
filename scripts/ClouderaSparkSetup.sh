042963bcb0429a552fd904d29e814ec1b28d305a
SrikalaRekapalli <srekapalli@sysgain.com>
2017-06-29 18:11:34 +0530

    Update ClouderaSparkSetup.sh

--- scripts/ClouderaSparkSetup.sh
+++ scripts/ClouderaSparkSetup.sh
@@ -4,9 +4,9 @@ MasterNode=$2
 InputFile=$3
 EndPoint="adl://datalaketd.azuredatalakestore.net"
 #Creating the Datalake Dirtectory
-hadoop fs -mkdir $EndPoint/$DataLakedir 
+hadoop fs -mkdir $EndPoint/$DataLakedir
 #Creating Cloudera user on hdfs cluster
 sudo -u hdfs hdfs dfs -mkdir /user/cloudera
 sudo -u hdfs hdfs dfs -chown cloudera:cloudera /user/cloudera
 wget -O /home/cloudera/ https://aztdrepo.blob.core.windows.net/clouderadirector/wordcount.jar
-spark-submit --master yarn --deploy-mode client --executor-memory 1g --jars /home/cloudera/wordcount.jar --conf spark.driver.userClasspathFirst=true --conf spark.executor.extraClassPath=/home/cloudera/wordcount.jar --class com.SparkWordCount.SparkWordCount /home/cloudera/wordcount.jar hdfs://'${MasterNode}':8020/user/admin/$InputFile 0 $EndPoint/$DataLakedir/WordCount &
+spark-submit --master yarn --deploy-mode client --executor-memory 1g --jars /home/cloudera/wordcount.jar --conf spark.driver.userClasspathFirst=true --conf spark.executor.extraClassPath=/home/cloudera/wordcount.jar --class com.SparkWordCount.SparkWordCount /home/cloudera/wordcount.jar hdfs://$MasterNode:8020/user/admin/$InputFile 0 $EndPoint/$DataLakedir/WordCount &
