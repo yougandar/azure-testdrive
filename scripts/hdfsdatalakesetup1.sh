f1045aa81966ca96de5ee5fcf7078ed0a708d859
SrikalaRekapalli <srekapalli@sysgain.com>
2017-06-29 18:51:18 +0530

    Update hdfsdatalakesetup1.sh

--- scripts/hdfsdatalakesetup1.sh
+++ scripts/hdfsdatalakesetup1.sh
@@ -36,7 +36,7 @@ echo "Cloudera Hue Web UI URL: http://$MasterIP:8888" >> /home/cloudera/NodeDeta
 echo "Cloudera Hue Web UI Username/Password: admin/admin" >> /home/cloudera/NodeDetails
 echo "Your Datalake Directory for the testdrive: $datalakeDirName" >> /home/cloudera/NodeDetails
 echo "Your Datalake Endpoint for the testdrive: $datalakeEndPoint/$datalakeDirName" >> /home/cloudera/NodeDetails
-echo "Your Output Data files on Datalake for the testdrive:  $datalakeEndPoint/$datalakeDirName/wordCount"
+echo "Your Output Data files on Datalake for the testdrive:  $datalakeEndPoint/$datalakeDirName/wordCount" >> /home/cloudera/NodeDetails
 #curl -X POST -u admin:admin 'http://'${MangerIP}':7180/api/v1/clusters/'${ClusterName}'/services/'${HDFS}'/commands/restart'
 #sleep 60
 #curl -X POST -u admin:admin 'http://'${MangerIP}':7180/api/v1/clusters/'${ClusterName}'/commands/restart'
