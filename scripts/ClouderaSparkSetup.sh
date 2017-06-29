3523e0d909f49bba01d7605b9b784ce8468eff27
SrikalaRekapalli <srekapalli@sysgain.com>
2017-06-29 18:00:34 +0530

    Update ClouderaSparkSetup.sh

--- scripts/ClouderaSparkSetup.sh
+++ scripts/ClouderaSparkSetup.sh
@@ -1,4 +1,3 @@
-
 #/bin/sh
 DataLakedir=$1
 MasterNode=$2
@@ -11,8 +10,3 @@ sudo -u hdfs hdfs dfs -mkdir /user/cloudera
 sudo -u hdfs hdfs dfs -chown cloudera:cloudera /user/cloudera
 wget -O /home/cloudera/ https://aztdrepo.blob.core.windows.net/clouderadirector/wordcount.jar
 spark-submit --master yarn --deploy-mode client --executor-memory 1g --jars /home/cloudera/wordcount.jar --conf spark.driver.userClasspathFirst=true --conf spark.executor.extraClassPath=/home/cloudera/wordcount.jar --class com.SparkWordCount.SparkWordCount /home/cloudera/wordcount.jar hdfs://'${MasterNode}':8020/user/admin/$InputFile 0 $EndPoint/$DataLakedir/WordCount &
-
-
-
-
-
